b88018ceea520e4d7722185c24a936c2ba04d032
William Wright <162603673+wwrigh18@users.noreply.github.com>
2025-10-09 18:56:49 +0000

    Defined test_given_conflict_input_then_merged_correctly and made sure to test that the anonymizer called the debug function, task 5 complete

--- presidio-anonymizer/tests/test_anonymizer_engine.py
+++ presidio-anonymizer/tests/test_anonymizer_engine.py
@@ -278,7 +278,17 @@ from unittest import mock
 @mock.patch("presidio_anonymizer.anonymizer_engine.logger")
 def test_given_conflict_input_then_merged_correctly(mock_logger):
     # replace the following `pass` line with your test implementation
-    pass
+    engine = AnonymizerEngine()
+    text = "I'm George Washington Square Park."
+    original_analyzer_results = [
+        RecognizerResult(start=4, end=21, entity_type="PERSON", score=1.0),
+        RecognizerResult(start=4, end=33, entity_type="LOCATION", score=1.0),
+    ]
+    anonymizer_result = engine.anonymize(
+        text,
+        original_analyzer_results
+    )
+    assert anonymizer_result.text == "I'm <LOCATION>."
 
 
 def _operate(
